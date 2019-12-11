ed274a38dc09ef8451d1da819792f934c0b6ce82
Todd Ren <c915384165@outlook.com>
2019-12-11 10:49:21 +0800

    add new api

--- read_write/example_11/AutoTraceDraw.py
+++ read_write/example_11/AutoTraceDraw.py
@@ -1,24 +1,58 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
 import turtle as t
+# 初始化
 t.title("自动轨迹绘制")
 t.setup(800, 600, 0, 0)
 t.pencolor("red")
 t.pensize(5)
 
 # 数据读取
-datals = []
-f = open('data.txt')
-for line in f:
-    line = line.replace("\n", "")
-    datals.append(list(map(eval, line.split(","))))
-f.close
+
+
+def read_data(file_name):
+    datals = []
+    f = open(file_name)
+    for line in f:
+        line = line.replace("\n", "")
+        datals.append(list(map(eval, line.split(","))))
+    f.close
+    return datals
+
+
+# 接口：
+# 0:前进；1: 方向<0> left, <1> right; 2: angle value; 3&4&5: RGB color;6: type of  line or circle
 # 自动绘制
-for i in range(len(datals)):
-    t.pencolor(datals[i][3], datals[i][4], datals[i][5])
-    t.fd(datals[i][0])
-    if datals[i][1]:
-        t.right(datals[i][2])
-    else:
-        t.left(datals[i][2])
+
+
+def draw_line(datals):
+    for i in range(len(datals)):
+        if datals[i][7] == 1:
+            if datals[i][6] == 0:
+                t.pencolor(datals[i][3], datals[i][4], datals[i][5])
+                t.fd(datals[i][0])
+                if datals[i][1]:
+                    t.right(datals[i][2])
+                else:
+                    t.left(datals[i][2])
+            # wait for
+            elif datals[i][6] == 1:
+                t.pencolor(datals[i][3], datals[i][4], datals[i][5])
+                if datals[i][1]:
+                    t.circle(datals[i][0], datals[i][2])
+                else:
+                    t.circle(datals[i][0], datals[i][2]*(-1))
+            else:
+                pass
+        else:
+            pass
+
+
+
+
+def main():
+    datals = read_data('data.txt')
+    draw_line(datals)
+
+
+main()
